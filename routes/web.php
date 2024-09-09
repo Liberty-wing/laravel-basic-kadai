829511cf0cf4df3c6f06765276b3f11b0e827a08
edmsh <exclamation0816@gmail.com>
2024-09-09 21:02:37 +0900

    no message

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/hello', function () {
-    return 'Hello,world!';
-});
+Route::get('/posts', [PostController::class, 'index']);
